a42aa08ce0654badb84949f9b65f6eaa4f7dccc0
Ahmed Nour Eldeen <105208779+ahmednour3123@users.noreply.github.com>
2023-08-28 17:30:23 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import ToDo from './components/to do/to-do';
+import Userform from './components/regrest/user-form';
+
+
+function App() {
+  return (
+    <div className="App">
+<ToDo/>
+<Userform/>
+    </div>
+  );
+}
+
+export default App;
